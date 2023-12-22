4e63f7643692866994ae416784ee3a177eb69ae8
NattawatC <64011478@kmitl.ac.th>
2023-12-22 21:15:48 +0700

    Create shutdown function

---
+++ Shutdown.py
@@ -0,0 +1,15 @@
+def shutdown_menu():
+    while True:
+        print("press Q to quit")
+        # print("press B to go back to main menu")
+        choice = input("Please enter your choice: ")
+        
+        if choice == "Q" | choice == "q" :
+            print("Quitting...")
+            break #shutdown the system
+        # elif chioce == "B" | choice == "b":
+        #     print("Going back to main menu...")
+        #     break
+
+if __name__ == "__main__":
+    shutdown_menu()
