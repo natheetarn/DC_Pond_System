fe5f6488fa9c3f4e19fa59aa363e1c294141ef26
NattawatC <64011478@kmitl.ac.th>
2023-12-22 21:34:29 +0700

    Create a fish method

---
+++ Fish.py
@@ -0,0 +1,15 @@
+def addFish(self):
+    # Implement the add_fish method logic here
+    pass
+
+def moveFish(self, fish_id, pond_id, port):
+    # Implement the move_fish method logic here
+    pass
+
+def removeFish(self, fish_id):
+    # Implement the remove_fish method logic here
+    pass
+
+def eatOtherFish(self, fish_id):
+    # Implement the eatOtherFish method logic here
+    pass
