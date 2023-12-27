e2cf3bb1fc77d0cc4fb2026eadbdd2c921e70b6e
DelighteningPigeon <89842170+fillpattana@users.noreply.github.com>
2023-12-27 19:32:26 +0700

    logging in separate files

--- Shutdown.py
+++ Shutdown.py
@@ -3,13 +3,14 @@ def shutdown_menu():
         print("press Q to quit")
         # print("press B to go back to main menu")
         choice = input("Please enter your choice: ")
-        
-        if choice == "Q" or choice == "q" :
+
+        if choice == "Q" or choice == "q":
             print("Quitting...")
-            break #shutdown the system
+            break  # shutdown the system
         # elif chioce == "B" | choice == "b":
         #     print("Going back to main menu...")
         #     break
+
 
 if __name__ == "__main__":
     shutdown_menu()
---
+++ philpond2.py
@@ -0,0 +1,98 @@
+import socket
+import time
+import threading
+import json
+import logging
+
+MULTICAST_GROUP = "224.1.1.1"
+MULTICAST_PORT = 5008
+INTERVAL = 2
+STATE_FILE = "pond2_state.json"
+LOG_FILE = "pond2_log.txt"
+
+
+def setup_logging():
+    logging.basicConfig(
+        filename=LOG_FILE,
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+    )
+
+
+def save_state(state):
+    with open(STATE_FILE, "w") as file:
+        json.dump(state, file)
+
+
+def load_state():
+    try:
+        with open(STATE_FILE, "r") as file:
+            state = json.load(file)
+        return state
+    except (FileNotFoundError, json.JSONDecodeError):
+        return None
+
+
+def listen_to_signal(state):
+    multicast_socket = socket.socket(
+        socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP
+    )
+    multicast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    multicast_socket.bind(("", MULTICAST_PORT))
+    multicast_socket.setsockopt(
+        socket.IPPROTO_IP,
+        socket.IP_ADD_MEMBERSHIP,
+        socket.inet_aton(MULTICAST_GROUP) + socket.inet_aton("0.0.0.0"),
+    )
+
+    multicast_socket.settimeout(5)
+    try:
+        while True:
+            data, _ = multicast_socket.recvfrom(1024)
+            decoded_data = data.decode()
+            print(f"Received: {decoded_data}")
+            logging.info(f"Received: {decoded_data}")
+    except socket.timeout:
+        print("No signal received within 5 seconds.")
+        logging.warning("No signal received within 5 seconds.")
+        set_up(state)
+
+
+def set_up(state):
+    print("SETTING UP AS SECOND POND")
+    logging.info("SETTING UP AS SECOND POND")
+    state["is_set_up"] = True
+    save_state(state)
+
+
+def send_multicast():
+    multicast_send_socket = socket.socket(
+        socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP
+    )
+    multicast_send_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
+
+    while True:
+        current_time = time.strftime("%H:%M:%S", time.localtime())
+        message = f"Current time2: {current_time}"
+        multicast_send_socket.sendto(
+            message.encode(), (MULTICAST_GROUP, MULTICAST_PORT)
+        )
+        print("Sent: ", message)
+        logging.info("Sent: %s", message)
+        time.sleep(INTERVAL)
+
+
+if __name__ == "__main__":
+    setup_logging()
+    state = load_state() or {"is_set_up": False}
+    if not state["is_set_up"]:
+        set_up(state)
+        state["is_set_up"] = True
+        save_state(state)
+
+    listen_thread = threading.Thread(target=listen_to_signal, args=(state,))
+    listen_thread.start()
+
+    time.sleep(10)
+    send_thread = threading.Thread(target=send_multicast)
+    send_thread.start()
